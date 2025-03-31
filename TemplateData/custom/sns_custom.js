284fa064ac5cb6edd57ccfdace4afaf6573ee142
ManthanBR <155616252+ManthanBR@users.noreply.github.com>
2025-03-31 13:37:43 +0530

    commit

--- TemplateData/custom/sns_custom.js
+++ TemplateData/custom/sns_custom.js
@@ -1,8 +1,8 @@
-let unityReady = false;
-let startARButtonListenerAdded = false;
+let recorder;
 let recording = false;
 let startTime;
-const maxRecordingTime = 30; // Set your desired max recording time in seconds
+let unityReady = false;
+let startARButtonListenerAdded = false;
 
 console.log("sns_custom.js: Script start");
 
@@ -12,11 +12,10 @@ function setupEventListeners() {
     if (recordButton) {
         console.log("recordButton found!");
         recordButton.addEventListener('click', function () {
-            if (!recording) {
-                startRecording();
-            }
-            else {
+            if (recording) {
                 stopRecording();
+            } else {
+                startRecording();
             }
         });
     } else {
@@ -25,36 +24,75 @@ function setupEventListeners() {
 }
 
 window.addEventListener("load", function () {
-    console.log("sns_custom.js: Page loaded! Initializing");
-
-    // Hide the button first
-    const recordButtonContainer = document.getElementById('cameraButtonContainer');
-    if (recordButtonContainer) {
-        recordButtonContainer.style.opacity = 0;
-        setTimeout(() => {
-            recordButtonContainer.style.display = "none";
-        }, 500);
-
-        var progressBarFull = document.querySelector("#unity-progress-bar-full");
-        var interval = setInterval(() => {
-            if (progressBarFull.style.width == "100%") {
-                console.log("Unity progress bar completed!");
-                clearInterval(interval);
-                unityReady = true;
-                // Show the button only when the game has started (or allow access button is not visible)
-                if (document.getElementById("startARDiv").style.visibility != 'visible') {
-                    recordButtonContainer.style.display = "block";
-                    setTimeout(() => {
-                        recordButtonContainer.style.opacity = 1;
-                    }, 50);
-                }
-            }
-        }, 100);
+    console.log("sns_custom.js: Page loaded! Initializing recorder");
+    const canvas = document.querySelector("#unity-canvas");
+    if (!canvas) {
+        console.error("Unity Canvas not found");
+        return;
     }
-    else {
-        console.error("Could not find recordButtonContainer to set the visibility")
-    }
+    ZapparVideoRecorder.createCanvasVideoRecorder(canvas, {
+        audio: false,
+        useSharePrompt: false
+    }).then(rec => {
+        recorder = rec;
+        console.log("Zappar video recorder is ready");
+        // Hide the button first
+        const recordButtonContainer = document.getElementById('cameraButtonContainer');
+         if(recordButtonContainer){
+              recordButtonContainer.style.opacity = 0;
+                setTimeout(() => {
+                    recordButtonContainer.style.display = "none";
+                }, 500);
 
+              var progressBarFull = document.querySelector("#unity-progress-bar-full");
+              var interval = setInterval(() => {
+                  if (progressBarFull.style.width == "100%") {
+                      console.log("Unity progress bar completed!");
+                      clearInterval(interval);
+                      unityReady = true;
+                     // Show the button only when the game has started (or allow access button is not visible)
+                     if (document.getElementById("startARDiv").style.visibility != 'visible') {
+                           recordButtonContainer.style.display = "block";
+                         setTimeout(() => {
+                             recordButtonContainer.style.opacity = 1;
+                         }, 50);
+                     }
+                 }
+             }, 100);
+         }
+         else{
+           console.error("Could not find recordButtonContainer to set the visibility")
+          }
+
+        recorder.onComplete.bind(async (res) => {
+           ZapparSharing({
+                data: await res.asDataURL(),
+                fileNamePrepend: 'MyCustomVideo',
+                shareTitle: 'Check out my Video',
+                shareText: 'Recorded using FilterYouAR',
+              },
+                {
+                   buttonCloseAnchor: {
+                      width: '30px',
+                      height: '30px',
+                    }
+              },
+               {
+                 onClose: function () {
+                        console.log("Save and share closed! resetting progress bar");
+                       const circle = document.querySelector('.progress-ring__bar');
+                        if (circle) {
+                            circle.style.strokeDashoffset = circle.getAttribute('r') * 2 * Math.PI;
+                        } else {
+                            console.error("Could not find the progress-ring__bar to reset the offset")
+                      }
+                 }
+               }
+            );
+        });
+    }).catch(e => {
+        console.error("Failed to initialize recorder:", e);
+    })
     setupEventListeners();
 
     if (document.getElementById("startARButton") != null && !startARButtonListenerAdded) {
@@ -65,114 +103,98 @@ window.addEventListener("load", function () {
                 recordButtonContainer.style.display = "block";
                 setTimeout(() => {
                     recordButtonContainer.style.opacity = 1;
-                }, 50);
+               }, 50);
             }
         });
     }
 });
 
-
-function resetProgress() {
-    console.log("Video recording complete! resetting progress bar");
-    const circle = document.querySelector('.progress-ring__bar');
-    if (circle) {
-        circle.style.strokeDashoffset = circle.getAttribute('r') * 2 * Math.PI;
-    } else {
-        console.error("Could not find the progress-ring__bar to reset the offset")
+window.addEventListener("zappar-sns-ready", function () {
+    console.log("Zappar SNS ready event fired!");
+    const targetNode = document.getElementById('ZapparSnapshotContainer');
+    if (!targetNode) {
+        console.error("Zappar Snapshot container not found");
+        return;
     }
-}
+    const observer = new MutationObserver(function (mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                console.log("child list mutated, checking for download button");
+                const saveButton = document.getElementById('zapparSaveButton');
+                if (saveButton) {
+                    console.log("Save button found!");
+                    saveButton.removeAttribute('download');
+                    saveButton.setAttribute('download', 'MyCustomVideoName.mp4');
+                    console.log("Save button download attribute set to MyCustomVideoName.mp4");
+                    observer.disconnect();
+                }
+            }
+        }
+    });
+    observer.observe(targetNode, { childList: true, subtree: true });
+});
 
 function startRecording() {
-    recording = true;
-    startTime = Date.now();
-    var tapcontainer = document.getElementById('tap-container');
-    const recordButton = document.getElementById('recordButton');
-    if (recordButton) {
-        recordButton.classList.add('recording');
-        tapcontainer.style.opacity = 0;
+    if (recorder) {
+        console.log("Starting video recording without custom audio");
+        recording = true;
+        startTime = Date.now();
+         const recordButton =  document.getElementById('recordButton');
+         if(recordButton){
+              recordButton.classList.add('recording');
+         } else {
+            console.error("Could not find the recordButton to set the recording style");
+         }
+        updateProgress();
+        recorder.start();
     } else {
-        console.error("Could not find the recordButton to set the recording style");
+        console.error("Recorder not ready yet")
     }
-    updateProgress();
-
-    // Send Message to Unity to activate a button
-    if (window.unityInstance) {
-        window.unityInstance.SendMessage('ARCamera', 'OnStartRecording');
-        console.log("Sent 'OnStartRecording' message to Unity");
-    }
-    else {
-        console.error("Could not find the Unity Instance to send 'OnStartRecording' message");
-    }
-
-      // Set a timer to stop recording automatically after maxRecordingTime seconds
-        setTimeout(() => {
-             if(recording){
-                  stopRecording();
-             }
-    }, maxRecordingTime * 1000);
 }
-
 
 function stopRecording() {
-    recording = false;
-    const recordButton = document.getElementById('recordButton');
-    if (recordButton) {
-        recordButton.classList.remove('recording');
-    } else {
-        console.error("Could not find the recordButton to remove the recording style");
-    }
-    const circle = document.querySelector('.progress-ring__bar');
-    if (circle) {
-        circle.style.strokeDashoffset = circle.getAttribute('r') * 2 * Math.PI;
-    } else {
-        console.error("Could not find the progress-ring__bar to reset the offset")
-    }
+    if (recorder) {
+        console.log("Stopping video recording without custom audio");
+        recording = false;
+        const recordButton = document.getElementById('recordButton');
+          if(recordButton){
+             recordButton.classList.remove('recording');
+         } else {
+            console.error("Could not find the recordButton to remove the recording style");
+         }
+         const circle = document.querySelector('.progress-ring__bar');
+        if(circle){
+             circle.style.strokeDashoffset = circle.getAttribute('r') * 2 * Math.PI;
+        } else {
+            console.error("Could not find the progress-ring__bar to reset the offset")
+        }
 
-    if (window.unityInstance) {
-        window.unityInstance.SendMessage('ARCamera', 'OnStopRecording');
-        console.log("Sent 'OnStopRecording' message to Unity");
-    }
-    else {
-        console.error("Could not find the Unity Instance to send 'OnStopRecording' message");
+        recorder.stop();
+    } else {
+        console.error("Recorder not ready yet")
     }
 }
-
 
 function updateProgress() {
     if (!recording) return;
     const currentTime = Date.now();
     const elapsedTime = (currentTime - startTime) / 1000;
-    const percentage = elapsedTime / maxRecordingTime;
+    const maxTime = 60;
+    const percentage = elapsedTime / maxTime;
     if (percentage >= 1) {
-      stopRecording();
+        stopRecording();
     }
     else {
         const circle = document.querySelector('.progress-ring__bar');
-        if (circle) {
-            const circumference = circle.getAttribute('r') * 2 * Math.PI;
-            const offset = circumference * (1 - percentage);
-            circle.style.strokeDashoffset = offset;
-            requestAnimationFrame(updateProgress);
-        }
-        else {
-            console.error("Could not find the progress-ring__bar to update progress");
-        }
+          if(circle){
+                  const circumference = circle.getAttribute('r') * 2 * Math.PI;
+                  const offset = circumference * (1 - percentage);
+                 circle.style.strokeDashoffset = offset;
+                  requestAnimationFrame(updateProgress);
+           }
+        else{
+          console.error("Could not find the progress-ring__bar to update progress");
+         }
+
     }
-}
-
-function OnVideoDataURLReceived(dataURL) {
-    console.log("Video data URL received: " + dataURL);
-    resetProgress();
-     ShowVideoPreview(dataURL);
-}
-function ShowVideoPreview(videoURL) {
-    var videoPreviewDiv = document.getElementById('videoPreviewDiv');
-    videoPreviewDiv.style.opacity = 1;
-    videoPreviewDiv.style.visibility = 'visible';
-
-    const video = document.getElementById('videoPreview');
-    video.src = videoURL;
-    video.style.width = "80vw";
-    video.style.height = 80 / window.innerWidth * window.innerHeight + "vw";
-    console.log("video.src: " + video.src);
 }
